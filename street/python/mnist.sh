b1b0fa43de34b1a97039f1c7fa34d2becff2c1e2
dengdan <dengdan890730@163.com>
2017-03-24 18:57:49 +0800

    ...

--- street/python/mnist.sh
+++ street/python/mnist.sh
@@ -3,7 +3,12 @@ export CUDA_VISIBLE_DEVICES=0
 
 train_dir=/home/dengdan/temp_nfs/fsns-debug
 rm -rf $train_dir
-python vgsl_train.py --max_steps=100000000 --train_data=../data/train* \
-  --train_dir=$train_dir 
+python vgsl_train.py --max_steps=100000000 --train_data=../data/train* --train_dir=$train_dir 
 
+python vgsl_eval.py --num_steps=1000 
+  --eval_data=../data/validation/validation* \
+  --decoder=../testdata/charset_size=134.txt \
+  --eval_interval_secs=300 
+  --train_dir=$train_dir 
+  --eval_dir=$train_dir/eval
 
