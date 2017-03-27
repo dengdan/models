dc04c96b25e88aababd665135dc6e84d1ac8d20f
DanDeng <dengdan890730@163.com>
2017-03-27 19:00:25 +0800

    ...

--- street/python-pool2/mnist.sh
+++ street/python-pool2/mnist.sh
@@ -1,12 +1,13 @@
 #export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=3
 
 dump_dir=/home/dengdan/temp_nfs/
 name=fsns-fcn12s-145k-pool2
 train_dir=$dump_dir/$name
-python vgsl_train.py --max_steps=100000000 --train_data=../data/train* \
-  --train_dir=$train_dir --proc_name=$name --gm=0.1
+tensorboard --logdir=$train_dir --port=9000
 exit
+python vgsl_train.py --max_steps=100000000 --train_data=../data/train* \
+  --train_dir=$train_dir --proc_name=$name --gm=1
 
 python vgsl_eval.py --num_steps=1000 \
   --eval_data=/home/dengdan/github/models/street/data/validation* \
@@ -14,6 +15,5 @@ python vgsl_eval.py --num_steps=1000 \
   --eval_interval_secs=300 --train_dir=$train_dir --eval_dir=$train_dir/eval \
   --proc_name=$name
 
-tensorboard --logdir=$train_dir
 
 
