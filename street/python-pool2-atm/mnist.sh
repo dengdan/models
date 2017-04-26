9c5ff05b29587c5d27ac1472fc900b91a4e5b452
dengdan <dengdan890730@163.com>
2017-04-26 15:04:49 +0800

    ...

--- street/python-pool2-atm/mnist.sh
+++ street/python-pool2-atm/mnist.sh
@@ -1,15 +1,10 @@
 #export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 dump_dir=/home/dengdan/temp_nfs/
 name=fsns-fcn12s-atm
 train_dir=$dump_dir/$name
 
-
-python vgsl_train.py --max_steps=100000000 --train_data=../data/train* \
-  --train_dir=$train_dir --proc_name=$name --gm=0.2
-
-exit
 python vgsl_eval.py --num_steps=1000 \
   --eval_data=/home/dengdan/github/models/street/data/validation* \
   --decoder=../testdata/charset_size=134.txt \
@@ -17,5 +12,10 @@ python vgsl_eval.py --num_steps=1000 \
   --proc_name=$name
   
 exit
+python vgsl_train.py --max_steps=100000000 --train_data=../data/train* \
+  --train_dir=$train_dir --proc_name=$name --gm=0.2
+
+exit
+
 tensorboard --logdir=$train_dir --port=9000
 
